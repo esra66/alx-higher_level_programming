9753ab6f4f7fd151c92d068a796a7dfd1932d072
esra66 <esra66mhmed@gmail.com>
2023-10-30 17:37:13 +0200

    String manipulation

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print("First 3 letters: {0:3}".format(word))
-print("Last 2 letters: {7:9}".format(word))
-print("Middle word: {1:8".format(word))
+worfi3 = word[0:3]
+worla2 = word[-2:]
+middwo = word[1:-1]
+print("First 3 letters: {}".format(worfi3))
+print("Last 2 letters: {}".format(worla2))
+print("Middle word: {}".format(middwo))
