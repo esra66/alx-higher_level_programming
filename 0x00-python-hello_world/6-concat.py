297c291430f607a34f3534ad5e7e1b5741e1ead7
esra66 <esra66mhmed@gmail.com>
2023-10-30 13:09:11 +0200

    print 3 times a string

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-Full_string = str1 + " " + str2
-print(f"{Full_string}!")
+Full_string = str1 + " " + str2 + "!"
+print("Welcome to {}".format(Full_string))
