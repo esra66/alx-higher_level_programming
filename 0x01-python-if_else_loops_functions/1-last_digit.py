5b5e11a87c86b70312b6ee8c0e34d386945d1237
esra66 <esra66mhmed@gmail.com>
2023-10-31 23:20:10 +0200

    prints the ASCII alphabet, in lowercase,

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 import random
-
 number = random.randint(-10000, 10000)
-dig = abs(number) % 10
-
-if number < 0:
-    dig = -dig
-
-        print("Last digit of {} is {} and is ".format(number, dig), end="")
-
-        if dig > 5:
-            print("greater than 5")
-        elif dig == 0:
-            print("0")
-        else:
-            print("less than 6 and not 0")
+if (number > 0):
+    remainder = number % 10
+else:
+    remainder = number % -10
+            print('Last digit of', number, 'is', remainder, end=' ')
+            if (number > 5):
+                print('and is greater than 5')
+            elif(number == 0):
+                print('and is 0')
+            else:
+                print('and is less than 6 and not 0')
