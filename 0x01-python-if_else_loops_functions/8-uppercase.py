51e040d711d2f4092f1218f223e57cf8cbd15cc6
esra66 <esra66mhmed@gmail.com>
2023-11-01 22:52:58 +0200

    "Correct my code"

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-  for digit1 in range(0, 10):
-      for digit2 in range(digit1 + 1, 10):
-          if digit1 == 8 and digit2 == 9:
-              print("{}{}".format(digit1, digit2))
-          else:
-              print("{}{}".format(digit1, digit2), end=", ")
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 123:
+            print(chr(ord(c) - 32), end="")
+        else:
+            print(c, end="")
+    print()
