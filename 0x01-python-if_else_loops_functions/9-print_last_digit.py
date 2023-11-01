2de597d332a44291a5bf64cce64097c5dd7eed17
esra66 <esra66mhmed@gmail.com>
2023-11-02 01:03:19 +0200

    "Correct"

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
-def uppercase(text):
-    for char in text:
-        ascii_val = ord(char)
-        if 97 <= ascii_val <= 122:
-            char = chr(ascii_val - 32)
-        print(char, end='')
-
-    print()
+def print_last_digit(number):
+    exe = 0
+    if number < 0:
+        number *= -1
+    exe = 1
+    last = number % 10
+    if exe == 1:
+        number *= -1
+    print("{:d}".format(last), end="")
+    return last
