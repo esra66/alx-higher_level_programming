eb5017163d82582cf7bbe17972dabe0312c37743
esra66 <esra66mhmed@gmail.com>
2023-11-01 21:43:48 +0200

    "Correction for number 6"

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
+
         if i != 8 or j != 9:
-            print(f"{i}{j}, ", end="")
+            print("{0}{1}, ".format(i, j), end="")
         else:
-            print(f"{i}{j}")
+            print("{0}{1}".format(i, j))
