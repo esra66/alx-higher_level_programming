ae8c6e3f3150411638375aeaaf3bca4127cb3967
esra66 <esra66mhmed@gmail.com>
2023-11-02 17:40:29 +0200

    "Last try"

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    total = 0
     import sys
-    length = len(sys.argv)
-    for i in range(1, length):
-        total += int(sys.argv[i])
-        print(total)
+    count = len(sys.argv)
+    sum = 0
+    for i in range(count - 1):
+        sum += int(sys.argv[i + 1])
+        print("{}".format(sum))
