5a1688fcdbcfcd39b8efb751042b57cb70fc814e
esra66 <esra66mhmed@gmail.com>
2023-11-05 15:33:24 +0200

    #!/usr/bin/python3

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-     for x in range(len(sentence)):
-         if len(sentence) == 0:
-             return (None, None)
-         else:
-             return (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        return (None, None)
+    else:
+        return (len(sentence), sentence[0])
