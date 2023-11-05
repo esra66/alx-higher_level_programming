03de5ffa16313f8e27d7ea42cbe60c4e0ee9a1b1
esra66 <esra66mhmed@gmail.com>
2023-11-06 00:23:41 +0200

    C function that prints some basic info about Python lists.

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,114 +1,26 @@
-#include "lists.h"
+#include <stdio.h>
+#include <Python.h>
 
 /**
- *  * reverseList - reverses the second half of the list
- *   *
- *    * @secondHalfHead: head of the second half
- *     * Return: no return
+ *  * print_python_list_info - prints info about a python list
+ *   * @p: PyObject
+ *     *Return: void 
  */
-void reverseList(listint_t **secondHalfHead)
+void print_python_list_info(PyObject *p)
 {
-	listint_t *prev;
-	listint_t *current;
-	listint_t *next;
+	long int size, i;
+	PyListObject *list;
+	PyObject *item;
 
-	prev = NULL;
-	current = *secondHalfHead;
+	size = Py_SIZE(p);
+	printf("[*] Size of the Python List = %ld\n", size);
 
-	while (current != NULL)
+	list = (PyListObject *)p;
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
 	}
-
-	*secondHalfHead = prev;
 }
-
-/**
- *  * compareLists - compares each element of the list
- *   *
- *    * @firstHalfHead: head of the first half
- *     * @secondHalfHead: head of the second half
- *      * Return: 1 if they are equal, 0 if not
- */
-int compareLists(listint_t *firstHalfHead, listint_t *secondHalfHead)
-{
-	listint_t *temp1;
-	listint_t *temp2;
-
-	temp1 = firstHalfHead;
-	temp2 = secondHalfHead;
-
-	while (temp1 != NULL && temp2 != NULL)
-	{
-		if (temp1->n == temp2->n)
-		{
-			temp1 = temp1->next;
-			temp2 = temp2->next;
-		}
-		else
-		{
-			return (0);
-		}
-	}
-
-	if (temp1 == NULL && temp2 == NULL)
-	{
-		return (1);
-	}
-
-	return (0);
-}
-
-/**
- *  * isPalindrome - checks if a singly linked list is a palindrome
- *   *
- *    * @head: pointer to head of list
- *     * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
-int isPalindrome(listint_t **head)
-{
-	listint_t *slow, *fast, *prevSlow;
-	listint_t *secondHalf, *middle;
-	int isPalindrome;
-
-	slow = fast = prevSlow = *head;
-	middle = NULL;
-	isPalindrome = 1;
-
-	if (*head != NULL && (*head)->next != NULL)
-	{
-		while (fast != NULL && fast->next != NULL)
-		{
-			fast = fast->next->next;
-			prevSlow = slow;
-			slow = slow->next;
-		}
-
-		if (fast != NULL)
-		{
-			middle = slow;
-			slow = slow->next;
-		}
-
-		secondHalf = slow;
-		prevSlow->next = NULL;
-		reverseList(&secondHalf);
-		isPalindrome = compareLists(*head, secondHalf);
-
-		if (middle != NULL)
-		{
-			prevSlow->next = middle;
-			middle->next = secondHalf;
-		}
-		else
-		{
-			prevSlow->next = secondHalf;
-		}
-	}
-
-	return (isPalindrome);
-}
-
