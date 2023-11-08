16d4cbacf652c236e280ae8e9ea412076807fee7
esra66 <esra66mhmed@gmail.com>
2023-11-08 18:36:37 +0200

    function that computes the square value of all integers of a matrix.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = []
     for x in my_list:
         if x == search:
-           new_list.append(replace)
+            new_list.append(replace)
         else:
-           new_list.append(x)
+            new_list.append(x)
     return new_list
