4f866feb14bb6322f80922ff9605d32432853997
esra66 <esra66mhmed@gmail.com>
2024-04-16 21:48:30 +0200

    advanced tasks

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -17,7 +17,7 @@ def append_after(filename="", search_string="", new_string=""):
          for line in f:
              res_line += [line]
              if line.find(search_string) != -1:
-                res_line += [new_string]
+                 res_line += [new_string]
 
-      with open(filename, 'w', encoding="utf-8") as f:
-                  f.write("".join(res_line))
+     with open(filename, 'w', encoding="utf-8") as f:
+         f.write("".join(res_line))
