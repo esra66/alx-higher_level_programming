2b8752ef7ae5fbabca8ac2cb186f0da4c6720b67
esra66 <esra66mhmed@gmail.com>
2024-04-16 22:26:02 +0200

    a function that creates an Object from a “JSON file”:

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,18 +1,12 @@
-
-dule that creates an Object from a JSON file
+#!/usr/bin/python3
 """
+function that creates an Object from a “JSON file”
+"""
+
 import json
 
 
 def load_from_json_file(filename):
-    """ Function that creates an Object from a JSON file
-
-    Args:
-        filename: textfile name
-
-    Raises:
-        Exception: when the object can't be encoded
-
-    """
-    with open(filename, 'r', encoding="utf-8") as f:
+    """crate and object from a "JSON file" """
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
